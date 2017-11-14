73535c6bb8733986c266b865a0cd0c8afa1bf1b3
Kyle-Althea <kyle-althea_santos@dlsu.edu.ph>
2017-11-14 17:14:53 +0800

    Initial Commit

---
+++ src/Demaru.java
@@ -0,0 +1,6 @@
+
+public class Demaru {
+	public Demaru(){
+		
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("First word: ");
+		String first = sc.nextLine();
+		System.out.print("Second word: ");
+		String second = sc.nextLine();
+		Demaru d = new Demaru();
+	}
+
+}
