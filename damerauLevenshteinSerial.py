dacbf85d5c1de3257449f36635441d8dc21b2306
Darlene Psalm Marpa <darlene_marpa@dlsu.edu.ph>
2017-12-10 20:56:47 +0800

    Adds capitalization of string before processing

--- damerauLevenshteinSerial.py
+++ damerauLevenshteinSerial.py
@@ -2,10 +2,6 @@
     Author: Darlene Marpa, Kyle-Althea Santos
 '''
 
-# INPUT #
-sourceString = input("Enter source sequence: ")
-comparingString = input("Enter comparing sequence: ")
-
 # Creates a rows x cols multidimential array with all elements initialized to 0 #
 class multidimen_array(list):
     def __init__(self, rows, cols=-1, blank=None):
@@ -41,6 +37,7 @@ def damerauLevenshteinDistance(a, b):
     maxDist = len(a) + len(b) + 1                       # computes for maximum possible distance
     h = multidimen_array(len(a) + 1, len(b) + 1, 0)     # assures all array are in same length (Sigma parameter)
 
+    # initializing matrix values #
     for i in range(0, len(a) + 1):
         h[i + 1][1] = i
         h[i + 1][0] = maxDist
@@ -48,35 +45,52 @@ def damerauLevenshteinDistance(a, b):
         h[1][j + 1] = j
         h[0][j + 1] = maxDist
 
-    da = [0 for x in range(0, len(alphabet))]
+    # algorithm implementation #
+    da = [0 for x in range(0, len(alphabet))]           # new array of Sigma integers
     for i in range(1, len(a) + 1):
+        print ("Entering Iteration ", i, " ...")
         db = 0
         for j in range(1, len(b) + 1):
             i1 = da[alphabet[b[j - 1]]]
             j1 = db
-            cost = 1
-            if (a[i - 1] == b[j - 1]):
-                print ("MATCH FOUND!\n")
+
+            cost = 1                                    # if match not found, cost is 1
+            if (a[i - 1] == b[j - 1]):                  # else, cost is 0
+                print ("MATCH FOUND!")
                 cost = 0
                 db = j
 
-                substitutionScore   = h[i][j] + cost                                # substitution
-                print ("Substitution: ", substitutionScore, '\n')
-                insertionScore      = h[i + 1][j] + 1                               # insertion
-                print ("Insertion: ", insertionScore, '\n')
-                deletionScore       = h[i][j + 1] + 1                               # deletion
-                print ("Deletion: ", deletionScore, '\n')
-                transpositionScore  = h[i1][j1] + (i - i1 - 1) + 1 + (j - j1 - 1)   # transposition
-                print ("Transposition: ", transpositionScore, '\n')
+            substitutionScore   = h[i][j] + cost
+            insertionScore      = h[i + 1][j] + 1
+            deletionScore       = h[i][j + 1] + 1
+            transpositionScore  = h[i1][j1] + (i - i1 - 1) + 1 + (j - j1 - 1)
 
-                print ('\n')
-                h[i + 1][j + 1] = min(
-                    substitutionScore,
-                    insertionScore,
-                    deletionScore,
-                    transpositionScore
-                )
-                print ("MINIMUM: ", h[i + 1][j + 1])
+            print ("Substitution: ", substitutionScore)
+            print ("Insertion: ", insertionScore)
+            print ("Deletion: ", deletionScore)
+            print ("Tranposition: ", transpositionScore)
+
+            h[i + 1][j + 1] = min(  substitutionScore, insertionScore,
+                                    deletionScore, transpositionScore )
+            minimum = h[i + 1][j + 1]
+
+            print ("MINIMUM: ")
+            if(minimum == substitutionScore):
+                print ("Substitution")
+            if(minimum == insertionScore):
+                print ("Insertion")
+            if(minimum == deletionScore):
+                print ("Deletion")
+            if(minimum == transpositionScore):
+                print ("Tranposition")
 
         da[alphabet[a[i - 1]]] = i
     return h[len(a) + 1][len(b) + 1]
+
+# INPUT #
+sourceString = input("Enter source sequence: ")
+comparingString = input("Enter comparing sequence: ")
+
+distance = damerauLevenshteinDistance(sourceString.upper(), comparingString.upper())
+print("Distance = ", distance)
+
